1e5e3e5bcaed7a877845467bab5b7c27c1260434
7db9a <>
2023-11-24 00:32:37 -0500

    Saving progress.

--- scripts/container_screenshot.py
+++ scripts/container_screenshot.py
@@ -43,3 +43,39 @@ if __name__ == '__main__':
   #time.sleep(3)
   #screenshot = create_screenshot_with_grid(50)
   #screenshot.save('chrome-screenshot7.png')
+
+  ## Click 'Load unpacked'
+  #pyautogui.moveTo(100 , 175)
+  #pyautogui.click()
+  #time.sleep(3)
+  #screenshot = create_screenshot_with_grid(50)
+  #screenshot.save('chrome-screenshot8.png')
+
+  ## Click 'app' directory
+  #pyautogui.moveTo(50, 150)
+  #pyautogui.click()
+  #time.sleep(3)
+  #screenshot = create_screenshot_with_grid(50)
+  #screenshot.save('chrome-screenshot9.png')
+
+  ## Click 'dist-chrome-extension
+  #pyautogui.moveTo(200, 125)
+  #pyautogui.doubleClick()
+  #time.sleep(2)
+  #pyautogui.moveTo(350, 50)
+  #pyautogui.click()
+  #pyautogui.press('enter')
+  #time.sleep(3)
+  #screenshot = create_screenshot_with_grid(50)
+  #screenshot.save('chrome-screenshot10.png')
+
+  ## Click extension button
+
+
+  pyautogui.moveTo(890, 60)
+  pyautogui.click()
+  time.sleep(3)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chrome-screenshot11.png')
+
+  
