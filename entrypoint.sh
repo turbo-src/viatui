dda49f695f9680532c217931dfff09d80403b0fd
7db9a <>
2023-11-24 13:28:11 -0500

    Add entrypoint script (forgot).

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Start Xvfb
+Xvfb :1 -screen 0 1366x768x24 &
+
+# Wait for Xvfb to start
+while ! xdpyinfo -display :1 > /dev/null 2>&1; do
+  sleep 0.1
+done
+
+# Start x11vnc
+x11vnc -display :1 -forever -usepw -create &
+
+# Start Fluxbox (or any other window manager)
+fluxbox &
+
+# Set the DISPLAY environment variable
+export DISPLAY=:1
+
+# Start Chromium in the background
+chromium --no-sandbox --disable-dbus --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage &
+
+# Keep the script running
+exec "$@"
+
