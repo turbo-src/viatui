d3f2844dc06a7dd9e83f1c3b4b09d95ed962f0b0
7db9a <>
2023-12-09 11:39:29 -0500

    Seperate loading of extension, page login, and navigating dom page.
    Also cleaned up files a bit.

---
+++ scripts/login.py
@@ -0,0 +1,48 @@
+  # Make a new browser tab
+  pyautogui.hotkey('ctrl', 't')
+  time.sleep(3)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chromium-nix-screenshots/tsrc-12.png')
+
+  # Type in github url into bar
+  login_page = get_from_viatuix_config('viatuix.json', 'url2')
+  pyautogui.moveTo(200, 75)
+  pyautogui.click()
+  pyautogui.typewrite(login_page)
+  time.sleep(2)
+  pyautogui.press('enter')
+  time.sleep(5)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chromium-nix-screenshots/tsrc-13.png')
+
+  # Type in username
+  username = get_from_viatuix_config('viatuix.json', 'username')
+  pyautogui.moveTo(414, 353)
+  pyautogui.click()
+  pyautogui.typewrite(username)
+  time.sleep(5)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chromium-nix-screenshots/tsrc-14.png')
+
+  # Type in password
+  password = get_from_viatuix_config('viatuix.json', 'password')
+  pyautogui.moveTo(423, 425)
+  pyautogui.click()
+  pyautogui.typewrite(password)
+  time.sleep(5)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chromium-nix-screenshots/tsrc-15.png')
+
+  # Submit username and password
+  pyautogui.moveTo(502, 477)
+  pyautogui.click()
+  time.sleep(5)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chromium-nix-screenshots/tsrc-16.png')
+
+  # Don't save password
+  pyautogui.moveTo(700, 400)
+  pyautogui.click()
+  time.sleep(5)
+  screenshot = create_screenshot_with_grid(100)
+  screenshot.save('chromium-nix-screenshots/tsrc-17.png')
--- scripts/pyautogui_screenshot_container.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['DISPLAY'] = ':1'
-
-import pyautogui
-
-screenshot = pyautogui.screenshot()
-screenshot.save('container_screenshot.png')
